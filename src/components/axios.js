915500730e0572704de09d7792f4e73d81a8f707
Julia Topolska <101990409+jtopolska@users.noreply.github.com>
2023-03-18 15:43:36 +0200

    render backend link

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3200'
+    baseURL: 'https://travel-planner-th2t.onrender.com'
 });
 
 export default instance;
