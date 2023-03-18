70a253a3def854411fa1c9cd132d0b3cc5fe0d47
Julia Topolska <101990409+jtopolska@users.noreply.github.com>
2023-03-18 15:45:33 +0200

    comments deleted

--- src/components/FetchItem.js
+++ src/components/FetchItem.js
@@ -3,53 +3,44 @@ import axios from './axios';
 export const getAllItems = ( setAllItems ) => {
     axios.get('/')
     .then(({data}) => {
-        console.log(data);
         setAllItems(data);
     })
 }
 
 export const postItem = ( travelItem, setTravelItem, setAllItems ) => {
     axios.post('/post', { travelItem, done: false })
-    .then((data) => {
-        console.log('fetch post data', data)
+    .then(() => {
         setTravelItem('');
         getAllItems(setAllItems)
     })
-    .catch((err) => console.log('fetch post err', err));
 }
 
 export const checkItem = ( _id, setAllItems ) => {
     axios.post('/check', { _id, done: true })
-    .then((data) => {
-        console.log('fetch check data', data)
+    .then(() => {
         getAllItems(setAllItems)
     })
 }
 
 export const editItem = ( itemID, travelItem, setTravelItem, setAllItems, setEdit ) => {
     axios.post('/edit', { _id: itemID, travelItem, done: false })
-    .then((data) => {
-        console.log('fetch edit data', data);
+    .then(() => {
         setTravelItem('');
         setEdit(false);
         getAllItems(setAllItems);
     })
-    .catch((err) => console.log('fetch edit err', err));
 }
 
 export const deleteItem = ( _id, setAllItems ) => {
     axios.post('/delete', { _id })
-    .then((data) => {
-        console.log('fetch delete data', data);
+    .then(() => {
         getAllItems(setAllItems);
     })
-    .catch((err) => console.log('fetch edit err', err));
 }
 
 export const uncheckItem = ( _id, setAllItems ) => {
     axios.post('/uncheck', { _id, done: false })
-    .then((data) => {
-        console.log('fetch uncheck data', data)
+    .then(() => {
         getAllItems(setAllItems)
     })
 }
